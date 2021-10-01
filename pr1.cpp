c32503687b25396b2575ce6bfd15721e97624a11
sushant4452 <84269507+sushant4452@users.noreply.github.com>
2021-10-01 18:31:48 +0530

    Create pr1.cpp

---
+++ pr1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll  long long
+#define IOP freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define twhile ll t; cin >> t; while(t--)
+
+int solve() {
+
+	
+}
+int main(){
+		FIO
+		IOP
+		twhile{
+			 solve() ;
+		}
+
+		return 0;
+	}
